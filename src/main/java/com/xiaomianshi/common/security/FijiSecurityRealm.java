8ed8d6be31c47c44ffb62c73a658595afa281304
zhen.yu <iszhenyu@yeah.net>
2018-07-13 01:26:07 +0800

    ..

---
+++ src/main/java/com/xiaomianshi/common/security/FijiSecurityRealm.java
@@ -0,0 +1,20 @@
+package com.xiaomianshi.common.security;
+
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+
+/**
+ * Created by xiaoz on 2017/5/9.
+ */
+public class FijiSecurityRealm extends JdbcRealm {
+
+	public FijiSecurityRealm() {
+		this.setSaltStyle(SaltStyle.COLUMN);
+		this.setPermissionsLookupEnabled(true);
+		this.setAuthenticationQuery("select password_hash, password_salt from sys_user where username = ?");
+		this.setAuthenticationCachingEnabled(true);
+		this.setAuthenticationCacheName(SecurityCacheName.AUTHENTICATION_CACHE);
+		this.setAuthorizationCachingEnabled(true);
+		this.setAuthorizationCacheName(SecurityCacheName.AUTHORIZATION_CACHE);
+	}
+
+}
