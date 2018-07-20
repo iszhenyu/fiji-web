7abeb3f5fde99d2f5f3150e78b3974899eb4aa0f
zhen.yu <iszhenyu@yeah.net>
2018-07-21 00:18:50 +0800

    ..

---
+++ fiji-biz/src/main/java/tech/jianshuo/fiji/biz/dao/mapper/ResourceMapper.java
@@ -0,0 +1,11 @@
+package tech.jianshuo.fiji.biz.dao.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zhen.yu
+ * @since 2018/7/21
+ */
+@Mapper
+public interface ResourceMapper {
+}
