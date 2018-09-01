c05d4740ac1bff41eda988a753e32c6a32252d8a
zhen.yu <iszhenyu@yeah.net>
2018-09-02 00:24:07 +0800

    refact FileUtils and UrlUtils

--- fiji-common/src/main/java/tech/jianshuo/fiji/common/util/FileUtils.java
+++ fiji-common/src/main/java/tech/jianshuo/fiji/common/util/FileUtils.java
@@ -3,45 +3,56 @@ package tech.jianshuo.fiji.common.util;
 import java.io.File;
 import java.io.FileOutputStream;
 
-/**
-*/
-public class FileUtils {
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    /**
-     * 获取文件类型
-     * @param fileName
-     * @return
-     */
+/**
+ * @author zhen.yu
+ * Created on 2018-09-02
+ */
+public class FileUtils {
+    private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
+
     public static String getFileExtName(String fileName) {
-        if (fileName != null) {
-            int i = fileName.lastIndexOf('.');
-            if (i > -1) {
-                return fileName.substring(i + 1).toLowerCase();
-            } else {
-                return null;
-            }
-        } else {
+        if (StringUtils.isBlank(fileName)) {
             return null;
         }
+        int i = fileName.lastIndexOf('.');
+        if (i > -1) {
+            return fileName.substring(i + 1).toLowerCase();
+        }
+        return null;
     }
 
-    /**
-     * 上传文件
-     * @param fileBytes
-     * @param filePath
-     * @param fileName
-     * @throws Exception
-     */
-    public static void uploadFile(byte[] fileBytes, String filePath, String fileName)
-            throws Exception {
+    public static void saveUploadedFile(byte[] fileBytes, String filePath, String fileName) throws Exception {
         File targetFile = new File(filePath);
+        boolean createFilePathSuccess = true;
         if (!targetFile.exists()) {
-            targetFile.mkdirs();
+            createFilePathSuccess = targetFile.mkdirs();
         }
-        FileOutputStream out = new FileOutputStream(filePath + fileName);
+        if (!createFilePathSuccess) {
+            logger.error("failed to create file path: {}", filePath);
+            return;
+        }
+        FileOutputStream out = new FileOutputStream(join(filePath, fileName));
         out.write(fileBytes);
         out.flush();
         out.close();
     }
 
+    public static String join(String filePath, String fileName) {
+        StringBuilder builder = new StringBuilder();
+        if (filePath.endsWith(File.pathSeparator)) {
+            builder.append(filePath);
+        } else {
+            builder.append(filePath).append(File.pathSeparator);
+        }
+        if (fileName.startsWith(File.pathSeparator)) {
+            builder.append(fileName.substring(File.pathSeparator.length()));
+        } else {
+            builder.append(fileName);
+        }
+        return builder.toString();
+    }
 }
--- fiji-common/src/main/java/tech/jianshuo/fiji/common/util/UrlUtils.java
+++ fiji-common/src/main/java/tech/jianshuo/fiji/common/util/UrlUtils.java
@@ -1,6 +1,5 @@
 package tech.jianshuo.fiji.common.util;
 
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.slf4j.Logger;
@@ -14,34 +13,13 @@ public class UrlUtils {
     private static final Logger logger = LoggerFactory.getLogger(UrlUtils.class);
     private static final int HTTP_OK = 200;
 
-    public static synchronized boolean isConnect(String urlStr) {
-        int counts = 0;
-        if (urlStr == null || urlStr.length() <= 0) {
-            return false;
-        }
-        while (counts < 3) {
-            try {
-                URL url = new URL(urlStr);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                int state = con.getResponseCode();
-                if (state == HTTP_OK) {
-                    return true;
-                }
-                break;
-            } catch (Exception ex) {
-                counts++;
-            }
-        }
-        return false;
-    }
-
     public static String getDomainUrl(String urlStr) {
         String domainUrl = urlStr;
         try {
             URL url = new URL(urlStr);
             domainUrl = url.getProtocol() + "://" + url.getAuthority();
         } catch (Exception e) {
-            logger.error("getDomainUrl is erro,url :" + urlStr, e);
+            logger.error("parse domainUrl from url error, url is: {}", urlStr, e);
         }
         return domainUrl;
     }
@@ -52,7 +30,7 @@ public class UrlUtils {
             URL url = new URL(urlStr);
             host = url.getHost();
         } catch (Exception e) {
-            logger.error("getHost is erro,url :" + urlStr, e);
+            logger.error("parse host from url error, url is: {}", urlStr, e);
         }
         return host;
     }
