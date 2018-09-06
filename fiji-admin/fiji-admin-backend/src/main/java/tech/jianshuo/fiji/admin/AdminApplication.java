1d8e565c9e54418afd20a241cda9122d3012feb5
zhen.yu <iszhenyu@yeah.net>
2018-09-06 23:38:59 +0800

    ..

---
+++ fiji-admin/fiji-admin-backend/src/main/java/tech/jianshuo/fiji/admin/AdminApplication.java
@@ -0,0 +1,8 @@
+package tech.jianshuo.fiji.admin;
+
+/**
+ * @author zhen.yu
+ * Created on 2018-09-06
+ */
+public class AdminApplication {
+}
---
+++ fiji-admin/fiji-admin-backend/src/main/java/tech/jianshuo/fiji/admin/controller/AuthController.java
@@ -0,0 +1,8 @@
+package tech.jianshuo.fiji.admin.controller;
+
+/**
+ * @author zhen.yu
+ * Created on 2018-09-06
+ */
+public class AuthController {
+}
